4d05b1d97334c7568e7d0243cb3eed7c22ad4fa7
yafes <berhanbingol1@gmail.com>
2025-03-28 12:32:15 +0300

    add more comment for database.py

--- source/database.py
+++ source/database.py
@@ -5,9 +5,13 @@ from tortoise import Tortoise
 
 @asynccontextmanager
 async def init():
+    """Initialize the database connection."""
     try:
         await Tortoise.init(config=tortoise_orm)
+        # safe=True, will just log warnings instead of erroring out if tables already exist
         await Tortoise.generate_schemas(safe=True)
+        # Yield the connection
         yield
     finally:
+        # Close the connection, when the context manager is done
         await Tortoise.close_connections()
